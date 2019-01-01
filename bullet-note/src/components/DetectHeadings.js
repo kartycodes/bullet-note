b53c2414847eb5984e7793ccafbdfd18b137dab5
Kartik Subramani <ksubramani@outlook.com>
2019-01-01 14:57:04 -0500

    fixed bug in heading logic

--- bullet-note/src/components/DetectHeadings.js
+++ bullet-note/src/components/DetectHeadings.js
@@ -2,9 +2,12 @@
 import {EditorState} from 'draft-js';
 
 const headerMap = {
-    "# "   : "header-one",
-    "## "  : "header-two",
-    "### " : "header-three"
+    "# "     : "header-one",
+    "## "    : "header-two",
+    "### "   : "header-three",
+    "#### "  : "header-four",
+    "##### " : "header-five",
+    "###### ": "header-six"
  }
 
 export default function detectHeadings(editorState) {
@@ -15,13 +18,19 @@ export default function detectHeadings(editorState) {
 
         blocks.forEach(block => {
             var text = block.getText();
-            var headingRegex = /^(\#*\s)/g;
+            var headingRegex = /^(\#{1,6}\s)/g;
             var found = text.match(headingRegex);
             if (found) {
-                var headerType = headerMap[found[0]];
+                var headerType   = headerMap[found[0]];
                 var updatedBlock = block.set("type", headerType);
                 updatedBlocks[updatedBlock.getKey()] = updatedBlock;
                 count++;
+            } else {
+                if (block.getType().startsWith("header")) {
+                    var updatedBlock = block.set("type", "unstyled");
+                    updatedBlocks[updatedBlock.getKey()] = updatedBlock;
+                    count++;
+                }
             }
         });
         if (count > 0) {
